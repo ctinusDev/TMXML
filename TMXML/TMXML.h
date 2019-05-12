172c321f7feb159cf2c3d147d3f8db182509d90d
ctinus <ctaiglm@gmail.com>
2019-05-12 15:13:57 +0800

    upload

---
+++ TMXML/TMXML.h
@@ -0,0 +1,19 @@
+//
+//  TMXML.h
+//  TMXML
+//
+//  Created by ctinus on 2019/5/12.
+//  Copyright © 2019 ctinus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TMXML.
+FOUNDATION_EXPORT double TMXMLVersionNumber;
+
+//! Project version string for TMXML.
+FOUNDATION_EXPORT const unsigned char TMXMLVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TMXML/PublicHeader.h>
+#import <TMXML/TMXMLReader.h>
+
---
+++ TMXML/TMXMLReader.h
@@ -0,0 +1,24 @@
+//
+//  TMXMLReader.h
+//  TMXML
+//
+//  Created by ctinus on 2019/5/12.
+//  Copyright © 2019 ctinus. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+enum {
+    XMLReaderOptionsProcessNamespaces           = 1 << 0, // Specifies whether the receiver reports the namespace and the qualified name of an element.
+    XMLReaderOptionsReportNamespacePrefixes     = 1 << 1, // Specifies whether the receiver reports the scope of namespace declarations.
+    XMLReaderOptionsResolveExternalEntities     = 1 << 2, // Specifies whether the receiver reports declarations of external entities.
+};
+typedef NSUInteger XMLReaderOptions;
+
+@interface TMXMLReader : NSObject <NSXMLParserDelegate>
+
++ (NSDictionary *)dictionaryForXMLData:(NSData *)data error:(NSError **)errorPointer;
++ (NSDictionary *)dictionaryForXMLString:(NSString *)string error:(NSError **)errorPointer;
++ (NSDictionary *)dictionaryForXMLData:(NSData *)data options:(XMLReaderOptions)options error:(NSError **)errorPointer;
++ (NSDictionary *)dictionaryForXMLString:(NSString *)string options:(XMLReaderOptions)options error:(NSError **)errorPointer;
+@end
